c06225d8df3d129b8097cfc550b4201e6ce949de
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2024-05-03 11:32:49 +0300

    Добавьте файлы проекта.

---
+++ WS_LR11/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    
+    static void Main()
+    {
+        int currentDay = 1;
+
+        Day day = () =>
+        {
+            string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+            string nextDay = daysOfWeek[currentDay];
+
+            currentDay = (currentDay + 1) % 7;
+
+            return nextDay;
+        };
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(day());
+        }
+    }
+}
+delegate string Day();
